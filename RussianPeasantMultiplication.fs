c26d217fdafa42376536545a35dfdbba1e9fb24b
Darran Shepherd <dshepherd@neworbit.co.uk>
2015-03-06 20:28:57 +0000

    Tidied up and parameterised test option

--- RussianPeasantMultiplication.fs
+++ RussianPeasantMultiplication.fs
@@ -1,28 +1,27 @@
 ﻿module RussianPeasantMultiplication
 
 let private (|Even|Odd|) n =
-    if n % 2 = 0 then Even else Odd
+    if n &&& 1 = 1 then Odd else Even
 
 let private ascendingTuple x y =
     if x < y then (x, y) else (y, x)
 
-let rec private constructList tuple =
+let rec private constructListByHalvingAndDoubling tuple =
     match tuple with
     | (0, _) -> []
-    | (x, y) -> tuple :: constructList (x / 2, y * 2)
+    | (x, y) -> tuple :: constructListByHalvingAndDoubling (x >>> 1, y <<< 1)
 
 let rec private sumRightColumnWhereLeftIsOdd list =
     match list with
-    | [] -> 0
-    | (Even, _)    :: tail -> 0 + sumRightColumnWhereLeftIsOdd tail
     | (Odd, right) :: tail -> right + sumRightColumnWhereLeftIsOdd tail
+    | (Even, _)    :: tail -> 0     + sumRightColumnWhereLeftIsOdd tail
+    | [] -> 0
 
 let private correctSignForNegatives x y multiple =
-    if (x > 0 && y > 0 || x < 0 && y < 0) then multiple else -multiple
+    if ((x > 0) = (y > 0)) then multiple else -multiple
 
 let multiply x y =
-    ascendingTuple x y                  // Smallest number first to minimise iterations of recursion
-    |> constructList                    // Recursively construct the list by halving and doubling until the first column reaches 1
-    |> sumRightColumnWhereLeftIsOdd     // Sum all of the values in the second column where the value in the first is odd
-    |> abs                              // Take the absolute of the result (as it will be negative if the larger of the arguments is negative)
-    |> correctSignForNegatives x y      // Return the negative of the multiplication if the arguments have opposite sign
+    ascendingTuple (abs x) (abs y)       // Smallest number first to minimise iterations of recursion
+    |> constructListByHalvingAndDoubling // Recursively construct the list by halving and doubling until the first column reaches 1
+    |> sumRightColumnWhereLeftIsOdd      // Sum all of the values in the second column where the value in the first is odd
+    |> correctSignForNegatives x y       // Return the negative of the multiplication if the arguments have opposite sign
--- Tests.fs
+++ Tests.fs
@@ -1,49 +1,62 @@
 ﻿module Tests
 
+open NUnit.Framework
 open RussianPeasantMultiplication
 
-open NUnit.Framework
-
 [<Test>]
-let ``When 1 is multipled by 1 expect 1``() =
+let ``Identity for multiplication`` =
     Assert.AreEqual(1, multiply 1 1)
 
 [<Test>]
-let ``When 1 is multipled by 2 expect 2``() =
+let ``Simplest non-identity multiplication`` =
     Assert.AreEqual(2, multiply 1 2)
 
 [<Test>]
-let ``When 2 is multipled by 1 expect 2``() =
+let ``Checking for commutative propert by reversing argument order`` =
     Assert.AreEqual(2, multiply 2 1)
 
 [<Test>]
-let ``When 3 is multipled by 4 expect 12``() =
+let ``Smaller number odd, larger number even``() =
     Assert.AreEqual(12, multiply 3 4)
 
 [<Test>]
-let ``When 3 is multipled by 5 expect 15``() =
+let ``Both numbers odd``() =
     Assert.AreEqual(15, multiply 3 5)
 
 [<Test>]
-let ``When 4 is multipled by 6 expect 24``() =
+let ``Both numbers even``() =
     Assert.AreEqual(24, multiply 4 6)
 
 [<Test>]
-let ``When 4 is multipled by 7 expect 28``() =
+let ``Smaller number even, larger number odd``() =
     Assert.AreEqual(28, multiply 4 7)
 
 [<Test>]
-let ``When 18 is multipled by 23 expect 414``() =
+let ``First argument negative, second positive``() =
+    Assert.AreEqual(-10, multiply -2 5)
+
+[<Test>]
+let ``First argument positive, second negative``() =
+    Assert.AreEqual(-12, multiply 3 -4)
+
+[<Test>]
+let ``Both arguments negative``() =
+    Assert.AreEqual(24, multiply -4 -6)
+
+[<Test>]
+let ``The problem example``() =
     Assert.AreEqual(414, multiply 18 23)
 
-[<Test>]
-let ``When -18 is multipled by 23 expect -414``() =
-    Assert.AreEqual(-414, multiply -18 23)
-
-[<Test>]
-let ``When 18 is multipled by -23 expect -414``() =
-    Assert.AreEqual(-414, multiply 18 -23)
-
-[<Test>]
-let ``When -18 is multipled by -23 expect 414``() =
-    Assert.AreEqual(414, multiply -18 -23)
+[<TestCase(1,   1, 1)>]   // Identity for multiplication
+[<TestCase(1,   2, 2)>]   // Simplest non-identity multiplication
+[<TestCase(2,   1, 2)>]   // Checking for commutative propert by reversing argument order
+[<TestCase(3,   4, 12)>]  // Smaller number odd, larger number even
+[<TestCase(3,   5, 15)>]  // Both numbers odd
+[<TestCase(4,   6, 24)>]  // Both numbers even
+[<TestCase(4,   7, 28)>]  // Smaller number even, larger number odd
+[<TestCase(-2,  5,-10)>]  // First argument negative, second positive
+[<TestCase(3,  -4,-12)>]  // First argument positive, second negative
+[<TestCase(-4, -6, 24)>]  // Both arguments negative
+[<TestCase(18, 23, 414)>] // The problem example
+let ``When x is multipled by y expect expected`` x y expected =
+    Assert.AreEqual(expected, multiply x y)
