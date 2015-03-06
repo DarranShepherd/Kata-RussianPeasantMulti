876bcabb564d0f428043da0cbb64f7180551b5d1
Darran Shepherd <dshepherd@neworbit.co.uk>
2015-03-06 19:43:26 +0000

    Working for positive numbers only

---
+++ RussianPeasantMultiplication.fs
@@ -0,0 +1,21 @@
+﻿module RussianPeasantMultiplication
+
+let private (|Even|Odd|) n =
+    if n % 2 = 0 then Even else Odd
+
+let private ascendingTuple x y =
+    if x < y then (x, y) else (y, x)
+
+let rec private constructList tuple =
+    match tuple with
+    | (0, _) -> []
+    | (x, y) -> tuple :: constructList (x / 2, y * 2)
+
+let rec private sumRightColumnWhereLeftIsOdd list =
+    match list with
+    | [] -> 0
+    | (Even, _)    :: tail -> 0 + sumRightColumnWhereLeftIsOdd tail
+    | (Odd, right) :: tail -> right + sumRightColumnWhereLeftIsOdd tail
+
+let multiply x y =
+    ascendingTuple x y |> constructList |> sumRightColumnWhereLeftIsOdd
---
+++ Tests.fs
@@ -0,0 +1,37 @@
+﻿module Tests
+
+open RussianPeasantMultiplication
+
+open NUnit.Framework
+
+[<Test>]
+let ``When 1 is multipled by 1 expect 1``() =
+    Assert.AreEqual(1, multiply 1 1)
+
+[<Test>]
+let ``When 1 is multipled by 2 expect 2``() =
+    Assert.AreEqual(2, multiply 1 2)
+
+[<Test>]
+let ``When 2 is multipled by 1 expect 2``() =
+    Assert.AreEqual(2, multiply 2 1)
+
+[<Test>]
+let ``When 3 is multipled by 4 expect 12``() =
+    Assert.AreEqual(12, multiply 3 4)
+
+[<Test>]
+let ``When 3 is multipled by 5 expect 15``() =
+    Assert.AreEqual(15, multiply 3 5)
+
+[<Test>]
+let ``When 4 is multipled by 6 expect 24``() =
+    Assert.AreEqual(24, multiply 4 6)
+
+[<Test>]
+let ``When 4 is multipled by 7 expect 28``() =
+    Assert.AreEqual(28, multiply 4 7)
+
+[<Test>]
+let ``When 18 is multipled by 23 expect 414``() =
+    Assert.AreEqual(414, multiply 18 23)
